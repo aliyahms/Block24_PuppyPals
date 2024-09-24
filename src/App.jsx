da2de04591cfedb644811b4f3dcab43e6c6f2118
Aliyah M Smith <aliyahms@umich.edu>
2024-09-24 09:48:14 -0400

    Completed the Use of the Data in React. Added console log to the App component. Imported useState and puppyList from our own data.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import { puppyList } from "./data.js";
 
 function App() {
+  console.log(puppyList);
+
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
   return (
     <>
       <div></div>
