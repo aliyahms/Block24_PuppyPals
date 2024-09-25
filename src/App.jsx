67808f1e01941d6b082725fac1393eac48b5366e
Aliyah M Smith <aliyahms@umich.edu>
2024-09-24 22:16:31 -0400

    We have a list of puupies displayed that on click will tell console its id and properties

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import { useState } from "react";
 import { puppyList } from "./data.js";
+import "./App.css";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
   console.log("puppyList: ", puppyList);
+
+  function selectPuppy(id) {
+    const puppy = puppies.find((p) => p.id === id);
+    setFeatPupId(puppy);
+
+    console.log("puppy id: ", puppy.id);
+  }
+
+  const $puppies = (
+    <div className="puppies">
+      {puppies.map((puppy) => (
+        <p key={puppy.id} onClick={() => selectPuppy(puppy.id)}>
+          {puppy.name}
+        </p>
+      ))}
+    </div>
+  );
+
   return (
     <>
-      <div className="App">
-        {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
-        })}
-      </div>
+      <h2>Puppies</h2>
+      {$puppies}
     </>
   );
 }
